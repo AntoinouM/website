244f7b40d7271a2580a28cba03e975c0a13a4468
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-07-03 09:36:19 +0200

    test mixins

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,13 @@
 export default defineNuxtConfig({
-  css: ['~/assets/scss/main.scss']
+  css: ['~/assets/scss/main.scss'],
+  compatibilityDate: '2024-07-02',
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/scss/_variables.scss" as *;'
+        }
+      }
+    }
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <div class="container">
+
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    
+    .container {
+        width: 100%;
+        height: 100vh;
+        background-color: $primary;
+    }
+
+</style>
