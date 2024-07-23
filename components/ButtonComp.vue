cefa65cfadfaffd07d3beda6fb098685f7b46246
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-07-23 13:53:15 +0200

    redo responsive section2

---
+++ components/ButtonComp.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+    const props = defineProps({
+        content:{
+            type: String,
+            required: true
+        },
+        btnType: {
+            type: String,
+            default: 'btn-primary'
+        },
+    })
+
+</script>
+
+<template>
+
+    <div>
+        <div
+            :class="props.btnType"
+            class="btn-container"
+        >
+            {{ props.content }}
+            <span class="icon">arrow_forward</span>
+        </div>
+    </div>
+    
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -19,14 +19,28 @@
                 ref="section2"
                 class="section section2"
             >
-                <GridContainer
-                    :nbCol="12"
-                    :nbRow="8"
-                >
-                    <p class="description">
-                        Welcome to Mind Retreats, where you'll discover the limitless power of your mind.<br/><br/>Our retreat is more than just a getaway; it is a transformative experience that will leave you feeling rejuvenated and empowered. <br/><br/>Surrounded by nature in a tranquil oasis, you'll find the peace needed to heal and actively transform your relationship with yourself, your thoughts, and your past experiences. A deep journey of self-discovery and healing. <br/><br/>Through hypnotherapy, daily morning movement, and private counselling in English and German, we explore the workings of the conscious and subconscious mind and how our past has shaped, beliefs, thought and behaviour patterns that persist today. We celebrate self-acceptance and self-love over the societal drive for self-optimization. <br/><br/>A skilled hypnotherapist will guide you through the retreat, creating a safe space where you can find peace, calmness, and transformation and who provides you with the support and guidance you need to embark on your pathway to connecting with yourself. 
-                    </p>
-                </GridContainer>
+                <div class="description">
+                    <p class="description-title title3">Welcome to Mind Retreats, where you'll discover the limitless power of your mind.</p>
+                    <p class="description-content body">Our retreat is more than just a getaway; it is a transformative experience that will leave you feeling rejuvenated and empowered. <br/><br/>Surrounded by nature in a tranquil oasis, you'll find the peace needed to heal and actively transform your relationship with yourself, your thoughts, and your past experiences. A deep journey of self-discovery and healing. <br/><br/>Through hypnotherapy, daily morning movement, and private counselling in English and German, we explore the workings of the conscious and subconscious mind and how our past has shaped, beliefs, thought and behaviour patterns that persist today. We celebrate self-acceptance and self-love over the societal drive for self-optimization. <br/><br/>A skilled hypnotherapist will guide you through the retreat, creating a safe space where you can find peace, calmness, and transformation and who provides you with the support and guidance you need to embark on your pathway to connecting with yourself.</p>
+                </div>
+                <div class="banner">
+                    <span class="block"></span>
+                    <ButtonComp
+                        class="btn btn-therapist"
+                        :content="'Discover the Therapist'"
+                        :btnType="'btn-primary'"
+                    />
+                    <ButtonComp
+                        class="btn btn-retreats"
+                        :content="'Discover the Retreats'"
+                        :btnType="'btn-primary'"
+                    />
+                </div>
+            </section>
+            <section 
+                ref="section3"
+                class="section section3"
+            >
 
             </section>
         </div>
@@ -41,7 +55,7 @@
         box-sizing: border-box;
         color: $black;
 
-        @include breakpoint-max-width(s) {
+        @include breakpoint-max-width(md) {
             height: fit-content;
         }
     }
