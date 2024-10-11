5aa4a9908e512da70b4c0720e1493f95ea059a29
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-10-11 12:24:38 +0200

    creation of accomodation page

---
+++ pages/accomodation.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <div class="accommodation-container">
+
+
+
+    </div>
+    
+</template>
+
+<style lang="scss" scoped>
+    
+    .accommodation-container {
+        width: 100%;
+        height: fit-content;
+        overflow-y: scroll;
+
+        background-color: $salmon-red;
+    }
+
+</style>
